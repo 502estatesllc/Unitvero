2b41afb1ff430a5bd782772ef12010b22e78c2dd
502estatesllc <devay95@outlook.com>
2026-09-17 10:03:34 -0400

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1232,7 +1232,7 @@ function isMultiFamily(property) {
                   })}
                 </div>
               </section>
-
+            )}
               <section className="activityShowcase">
                 <div className="showcaseHeader">
                   <div>
